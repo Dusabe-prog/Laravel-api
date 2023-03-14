3530d5149c4197062df2be8fc518d3f31fc39e7f
Dusabe-prog <dusabeeme55@gmail.com>
2023-03-14 14:42:59 +0200

    Making APIs exemple in Laravel

---
+++ app/Models/Customer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Invoice;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'type', 'email', 'address', 'city', 'state', 'postal_code',
+    ];
+
+    public function invoice()
+    {
+        return $this->hasMany(Invoice::class);
+    }
+}
